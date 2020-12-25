e7712eba539f36cafde1125c5b08acb7bec01e94
Hamzahsm <hamzahsaifulmadjid@gmail.com>
2020-12-25 15:16:05 +0700

    adding foto and editing file

---
+++ script.js
@@ -0,0 +1,4 @@
+const redButton = document.querySelector('.red').addEventListener('click', () => document.body.style.backgroundColor = 'red');
+const blueButton = document.querySelector('.blue').addEventListener('click', () => document.body.style.backgroundColor = 'blue');
+const blackButton = document.querySelector('.black').addEventListener('click', () => document.body.style.backgroundColor = 'black');
+const normalButton = document.querySelector('.normal').addEventListener('click', () => document.body.style.backgroundColor = 'white'); 
