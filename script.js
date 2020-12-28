afac239d3efb0e20aa38204d0a8f9134502c977b
Hamzahsm <hamzahsaifulmadjid@gmail.com>
2020-12-28 09:07:06 +0700

    editing my js

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const redButton = document.querySelector('.red').addEventListener('click', () =>
 const blueButton = document.querySelector('.blue').addEventListener('click', () => document.body.style.backgroundColor = 'blue');
 const blackButton = document.querySelector('.black').addEventListener('click', () => document.body.style.backgroundColor = 'black');
 const normalButton = document.querySelector('.normal').addEventListener('click', () => document.body.style.backgroundColor = 'white'); 
+
+console.log('hello world'); 
